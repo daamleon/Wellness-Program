08098a20321a7140d986e8734b91b6af939cd3e8
Daamleon <withdaamleon@gmail.com>
2025-02-13 15:34:15 +0700

    update

--- src/components/MainContent1.jsx
+++ src/components/MainContent1.jsx
@@ -7,18 +7,18 @@ function MainContent1() {
   return (
     <section className="bg-white shadow-lg rounded-xl mx-4 md:mx-auto max-w-6xl my-10 flex flex-col-reverse md:flex-row items-stretch">
       {/* Bagian Teks */}
-      <div className="md:w-3/5 p-6 flex flex-col justify-center">
-        <div className="flex items-center gap-4 mb-4">
+      <div className="w-full p-6 flex flex-col justify-center">
+        {/* <div className="flex items-center gap-4 mb-4">
           <img src={indonesiaLogo} alt="Wonderful Indonesia" className="h-8" />
           <img src={dailyHotelsLogo} alt="Daily Hotels" className="h-6" />
-        </div>
-        <h3 className="text-2xl md:text-3xl font-bold text-gray-900 leading-relaxed md:text-left text-justify">
+        </div> */}
+        <h3 className="text-2xl md:text-3xl font-bold text-gray-900 leading-relaxed md:text-center text-justify">
           Sebuah program 2 hari, 1 malam yang membantu kamu untuk{" "}
           <span className="text-pink-600">
             melepaskan stres berkepanjangan.
           </span>
         </h3>
-        <p className="text-gray-700 text-md md:text-lg mt-4 leading-relaxed text-justify md:text-left">
+        <p className="text-gray-700 text-md md:text-lg mt-4 leading-relaxed text-justify md:text-center">
           <strong>Wellness Tourism Program</strong> adalah kegiatan yang
           ditujukan bagi kamu yang ingin menghilangkan kepenatan sejenak.
           Difasilitasi oleh instruktur berpengalaman, program ini membantu kamu
@@ -28,13 +28,13 @@ function MainContent1() {
       </div>
 
       {/* Bagian Gambar */}
-      <div className="md:w-2/5 h-56 md:h-auto flex items-center justify-center">
+      {/* <div className="md:w-2/5 h-56 md:h-auto flex items-center justify-center">
         <img
           src={sleepTourismImage}
           alt="Sleep Tourism Program"
           className="w-full h-full object-cover rounded-t-xl md:rounded-l-none md:rounded-r-xl"
         />
-      </div>
+      </div> */}
     </section>
   );
 }
