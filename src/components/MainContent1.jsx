4e129ff10aa911bfeb2d40a9ea725c2f30bd1816
Daamleon <withdaamleon@gmail.com>
2025-02-13 15:40:06 +0700

    update

--- src/components/MainContent1.jsx
+++ src/components/MainContent1.jsx
@@ -12,13 +12,13 @@ function MainContent1() {
           <img src={indonesiaLogo} alt="Wonderful Indonesia" className="h-8" />
           <img src={dailyHotelsLogo} alt="Daily Hotels" className="h-6" />
         </div> */}
-        <h3 className="text-2xl md:text-3xl font-bold text-gray-900 leading-relaxed md:text-center text-justify">
+        <h3 className="text-2xl md:text-3xl font-bold text-gray-900 leading-relaxed text-center">
           Sebuah program 2 hari, 1 malam yang membantu kamu untuk{" "}
           <span className="text-pink-600">
             melepaskan stres berkepanjangan.
           </span>
         </h3>
-        <p className="text-gray-700 text-md md:text-lg mt-4 leading-relaxed text-justify md:text-center">
+        <p className="text-gray-700 text-md md:text-lg mt-4 leading-relaxed text-center">
           <strong>Wellness Tourism Program</strong> adalah kegiatan yang
           ditujukan bagi kamu yang ingin menghilangkan kepenatan sejenak.
           Difasilitasi oleh instruktur berpengalaman, program ini membantu kamu
