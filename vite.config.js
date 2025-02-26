8c22dd1d3a64adf2aed94cfadb738b84757d701f
Daamleon <withdaamleon@gmail.com>
2025-02-26 15:01:16 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/wellness-program/",
+  base: "/wellness-program",
   plugins: [react()],
   resolve: {
     alias: {
